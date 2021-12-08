5228999dcab8817e312fefa92989e7b9b9e720c9
kontrec91 <64136241+kontrec91@users.noreply.github.com>
2021-12-08 16:29:45 +0200

    Add files via upload
    adding active-filter and count tasks

--- js/script.js
+++ js/script.js
@@ -1,31 +1,90 @@
-let input = document.querySelectorAll(".input-goal")[0];
-let list = document.getElementsByTagName("ul")[0];
+let input = document.querySelector(".input-goal");
+let list = document.querySelector("ul");
+let dotoApp = document.querySelector('.dotoApp');
+let countItems = document.querySelector('.number-items-left');
+let allButton = document.querySelector('.all-button');
+let activeButton = document.querySelector('.active-button');
+let completedButton = document.querySelector('.completed-button');
+let clearCompletedButton = document.querySelector('.clear-completed-button');
 
 let dataArray = [];
 
-function showElements(array, event) {
-  let elemsNode = ``;
+function setArray(array=null, element = null) {
+  if(element == null && array !== null) {
+    dataArray=array;
+    showElements();
+    showCountItems();
+  }
+    // dataArray=array;
+    showElements(array);
+    showCountItems(array);
+};
 
-  array.forEach((element, index) => {
-    elemsNode += `<li id=${index} class='list-item'>
-                            <input type='checkbox' class='item-checkbox'>
-                            <label class='item-text'>${element.value}</label>
-                            <span class='delete'>X</span>
-                        </li>`;
-  });
-
-  list.innerHTML = elemsNode;
+function showCountItems(array = null){
+  if(array){
+    dataArray=array;
+  }
+      countItems.innerText = dataArray.length +' items left';
 }
 
-input.addEventListener("keypress", (e) => {
-  if (e.keyCode === 13 && e.target.value !== "") {
-    dataArray.push({
-      checked: false,
-      delete: false,
-      value: e.target.value,
-    });
+list.addEventListener("click", (event) => {
+  if (event.target.className === "item-checkbox") {
+      isChecked(event); 
+  }
+});
 
-    showElements(dataArray, e);
+function isChecked(event) {
+  let elem = dataArray.find((item) => item.id == event.target.parentElement.id);
+  elem.checked = event.target.checked;
+  console.log(event, dataArray);
+  event.target.checked
+    ? event.target.parentElement.children[1].classList.add("list-item-done")
+    : event.target.parentElement.children[1].classList.remove("list-item-done");
+}
+
+// render
+function showElements(array=null) {
+  console.log('array: ', array);
+    list.innerHTML ='';
+    if(array){
+      dataArray=array;
+    }
+    dataArray.forEach((element) => {
+    let li = document.createElement('li');
+    let checkBox = document.createElement('input');
+    let deleteItem = document.createElement('span');
+    let label = document.createElement('label');
+
+    if(element.checked == true){
+      label.setAttribute("class", "list-item-done")
+    }
+
+    checkBox.type = 'checkbox';
+    checkBox.setAttribute("class", "item-checkbox");
+    li.setAttribute("class", 'item-text');
+    li.setAttribute("id", element.id);
+
+    label.innerText = element.value;
+
+    deleteItem.innerText = 'X';
+    deleteItem.setAttribute('class', 'delete');
+
+    li.appendChild(label);
+    li.insertAdjacentElement('afterbegin', checkBox);
+    li.insertAdjacentElement('beforeend', deleteItem);
+
+    list.appendChild(li);
+  });
+};
+
+input.addEventListener("keypress", (e) => {
+  if (e.keyCode === 13 && e.target.value.trim() !== "") {
+
+   setArray([...dataArray, {
+      checked: false,
+      value: e.target.value,
+      id: Date.now()
+    }]);
 
     console.log(dataArray);
 
@@ -33,45 +92,122 @@ input.addEventListener("keypress", (e) => {
   }
 });
 
-function onHandleEvent(event) {
-  if (event.target.className === "delete") {
-    console.log(event.target.parentElement);
-    console.log(event.target.parentElement.id);
+list.addEventListener("click", (event) => {
+    if (event.target.className === "delete") {
+        const arr = dataArray.filter(item => item.id != event.target.parentElement.id);
+        console.log(arr)
+        setArray(arr)
+    }
+  });
 
-    dataArray.splice(event.target.parentElement.id, 1);
 
-    showElements(dataArray);
-  } else if (event.target.className === "item-checkbox") {
-    dataArray[event.target.parentElement.id].checked = event.target.checked
-      ? true
-      : false;
-    event.target.checked
-      ? event.target.parentElement.classList.add("list-item-done")
-      : event.target.parentElement.classList.remove("list-item-done");
-  } else if (event.target.className === "item-text") {
-    console.log(event);
-    console.log(event.target);
+  activeButton.addEventListener('click', (event) => {
+    activeTasks(event.target);
+  });
 
-    console.log(dataArray[event.target.parentElement.id].value);
-
-    let reducting = document.createElement("input");
-
-    event.target.addEventListener("click", (event) => {
-      event.target.appendChild(reducting);
-      reducting.addEventListener('onchange', (event) => {
-        dataArray[event.target.id].value = event.target.value; 
-      })
-    });
-    event.target.removeChild(reducting);
-
-    showElements(dataArray);
+  function activeTasks(element) {
+      let array = dataArray.filter((item) => item.checked == false);
+      setArray(array, element);
   }
-}
+
+  allButton.addEventListener('click', (event) => {
+    allTascks(event.target);
+  })
+
+  function allTascks(element) {
+    setArray(array = null, element);
+  }
+// dotoApp.addEventListener("click", (event) => {
+//     if(event.target.className === "delete") {
+
+//     }
+// });
+
+// function onHandleEvent(event) {
+//   if (event.target.className === "delete") {
+//       showElements(dataArray);
+//     dataArray.splice(event.target.parentElement.id, 1);
+
+
+//   } 
+//   else if (event.target.className === "item-checkbox") {
+//     dataArray[event.target.parentElement.id].checked = event.target.checked
+//       ? true
+//       : false;
+//     event.target.checked
+//       ? event.target.parentElement.classList.add("list-item-done")
+//       : event.target.parentElement.classList.remove("list-item-done");
+
+//   } 
+//   else if (event.target.className === "item-text") {
+//     console.log(event);
+//     console.log(event.target.textContent);
+
+//     // console.log(dataArray[event.target.parentElement.id].value);
+
+//     let reducting = document.createElement("input");
+
+//     event.target.addEventListener("onclick", (event) => {
+        
+//       event.target.parentElement.appendChild(reducting);
+//     //   event.target.parentElement.removeChild(event.target);
+
+//     //   reducting.addEventListener('onchange', (event) => {
+//     //     reducting.value = event.target.value; 
+//     //     dataArray[event.target.id].value = event.target.value; 
+//     //   })
+//     });
+//     // event.target.removeChild(reducting);
+
+//     event.target.parentElement.removeChild(reducting);
+//     // event.target.parentElement.appendChild(event.target);
+
+//     showElements(dataArray);
+//   }
+// }
 // function changeValue(index){
 //     console.log(index);
 // }
 
-list.addEventListener("click", (event) => {
-  console.log(event.target.className);
-  onHandleEvent(event);
+// list.addEventListener("click", (event) => {
+//   console.log(event.target.className);
+//   onHandleEvent(event);
+//   event.target.className === "item-text"? changeText(event) : null;
+// });
+
+
+
+
+list.addEventListener("dbclick", (event) => {
+    console.log(event.target.className);
+    changeText(event);
 });
+
+function changeText(event) {
+    console.log(event);
+    console.log(event.target.textContent);
+
+    // console.log(dataArray[event.target.parentElement.id].value);
+
+    let reducting = document.createElement("input");
+        
+    event.target.parentElement.appendChild(reducting);
+    reducting.classList.add('visible');
+    event.target.classList.add('hidden');
+
+
+    //   event.target.parentElement.removeChild(event.target);
+
+    //   reducting.addEventListener('onchange', (event) => {
+    //     reducting.value = event.target.value; 
+    //     dataArray[event.target.id].value = event.target.value; 
+    //   })
+    // event.target.removeChild(reducting);
+
+    reducting.classList.remove('visible');
+    event.target.classList.add('visible');
+
+    // event.target.parentElement.appendChild(event.target);
+
+    showElements(dataArray);
+}
